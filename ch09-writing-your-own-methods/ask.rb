7dc1bcb36c4e9cc3065263b898e86dd812773b0b
Jules Morley <jules.morley@gmail.com>
2015-07-15 16:59:25 +0100

    Morley Chris Pine ask.rb

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,27 @@
 def ask question
-  # your code here
+  def ask question
+	while true
+		puts question
+		reply = gets.chomp.downcase
+		if (reply == 'yes' || reply == 'no')
+			if reply == 'yes' 
+				return true
+			else 
+				return false
+			end
+		else puts 'Please answer "yes" or "no' 
+		end
+	end
 end
+
+
+	puts 'hello'
+	puts
+	ask 'Do you like tacos?'
+	ask 'Do you like burritos?'
+	wets_bed = ask 'Do you wet the bed?'
+	puts
+	puts "DEBRIEFING"
+	puts 'Thank you.....'
+	puts
+	puts wets_bed
