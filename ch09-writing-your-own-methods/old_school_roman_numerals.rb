faf3fa1c8ab4b4e8d46e725557d7fcd51ca01983
Jules Morley <jules.morley@gmail.com>
2015-07-16 12:44:04 +0100

    Update old_school_roman_numerals.rb

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,25 @@
-def old_roman_numeral num
-  # your code here
+while true
+	puts 'Give me an integer'
+	integer = gets.chomp.to_i
+
+	roman = ''
+
+	roman = "M" * (integer / 1000) 
+	integer = integer % 1000
+
+	roman += "D" * (integer / 500)
+	integer = integer % 500
+
+	roman += "C" * (integer / 100)
+	integer = integer % 100
+
+	roman += "L" * (integer / 50)
+	integer = integer % 50
+
+	roman += "X" * (integer / 10)
+	integer = integer % 10
+
+	roman += "I" * (integer)
+
+puts roman
 end
